2bf73bcf0f6835096644da4af69501f1b5c70a41
Gabriel Malafaia <gabrielbsnsp@gmail.com>
2023-03-30 07:15:46 -0300

    test: deploy

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,28 @@
-import { app } from "./app";
 import { prismaClient } from "./database/prismaClient";
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "express-async-errors";
+import "reflect-metadata";
+import { errorHandler } from "./errors";
+import { contactsRoutes } from "./routes/contacts.routes";
+import { sessionsRoutes } from "./routes/sessions.routes";
+import { usersRoutes } from "./routes/users.routes";
+
+const app = express();
+app.use(express.json());
+
+app.use(cors());
+app.use("/users", usersRoutes);
+app.use("/sessions", sessionsRoutes);
+app.use("/contacts", contactsRoutes);
+
+app.get("/", async (request: Request, response: Response) => {
+  const users = await prismaClient.user.findMany();
+
+  return response.json(users);
+});
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 process.env.DEBUG = "prisma:*";
@@ -14,3 +37,5 @@ prismaClient
     });
   })
   .catch((err) => console.error(err));
+
+export { app };
